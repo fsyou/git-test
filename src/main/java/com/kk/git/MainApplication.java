fda379321ca624b7eb165890afc984d0635f677f
fsyou <fs__you@163.com>
2021-04-26 16:15:31 +0800

    second commit

--- src/main/java/com/kk/git/MainApplication.java
+++ src/main/java/com/kk/git/MainApplication.java
@@ -3,5 +3,6 @@ package com.kk.git;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
